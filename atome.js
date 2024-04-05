5f16734a235cfc1add62adbb99534bc240c64367
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2024-04-05 09:53:26 +0200

    first commit

---
+++ atome.js
@@ -0,0 +1,83 @@
+import * as THREE from 'three';
+
+const atomSize = 0.02;
+
+export function createN2(){
+const geometry = new THREE.SphereGeometry(atomSize, 32, 32);
+
+const material = new THREE.MeshBasicMaterial({ color: 0x0000FF });
+
+const sphere1 = new THREE.Mesh(geometry, material);
+const sphere2 = new THREE.Mesh(geometry, material);
+
+sphere1.position.set(-atomSize/2, 0, 0);
+sphere2.position.set(atomSize/2, 0, 0);
+
+const molecule = new THREE.Group();
+molecule.add(sphere1);
+molecule.add(sphere2);
+
+return molecule;
+}
+
+export function createO2(){
+    const geometry = new THREE.SphereGeometry(atomSize, 16, 16);
+    
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    
+    const sphere1 = new THREE.Mesh(geometry, material);
+    const sphere2 = new THREE.Mesh(geometry, material);
+    
+    sphere1.position.set(-atomSize/2, 0, 0);
+    sphere2.position.set(atomSize/2, 0, 0);
+    
+    const molecule = new THREE.Group();
+    molecule.add(sphere1);
+    molecule.add(sphere2);
+    
+    return molecule;
+    }
+
+    export function createCO2(){
+        const geometry = new THREE.SphereGeometry(atomSize, 16, 16);
+    
+        const materialC = new THREE.MeshBasicMaterial({ color: 0x000000 });
+        const materialO = new THREE.MeshBasicMaterial({ color: 0xFf0000 });
+    
+        const sphereC = new THREE.Mesh(geometry, materialC);
+        const sphereO1 = new THREE.Mesh(geometry, materialO);
+        const sphereO2 = new THREE.Mesh(geometry, materialO);
+    
+        sphereC.position.set(0, 0, 0);
+        sphereO1.position.set(-atomSize, 0, 0);
+        sphereO2.position.set(atomSize, 0, 0);
+    
+        const molecule = new THREE.Group();
+        molecule.add(sphereC);
+        molecule.add(sphereO1);
+        molecule.add(sphereO2);
+    
+        return molecule; // Retournez le groupe
+    }
+
+    export function createH20(){
+        const geometry = new THREE.SphereGeometry(atomSize, 16, 16);
+    
+        const materialH = new THREE.MeshBasicMaterial({ color: 0xFFFFFF , transparent: true, opacity: 1 });
+        const materialO = new THREE.MeshBasicMaterial({ color: 0xFF0000, transparent: true, opacity: 1});
+    
+        const sphereC = new THREE.Mesh(geometry, materialO);
+        const sphereO1 = new THREE.Mesh(geometry, materialH);
+        const sphereO2 = new THREE.Mesh(geometry, materialH);
+    
+        sphereC.position.set(0, 0, 0);
+        sphereO1.position.set(-atomSize, 0, 0);
+        sphereO2.position.set(atomSize, 0, 0);
+    
+        const molecule = new THREE.Group();
+        molecule.add(sphereC);
+        molecule.add(sphereO1);
+        molecule.add(sphereO2);
+    
+        return molecule; // Retournez le groupe
+    }
---
+++ main.js
@@ -0,0 +1,227 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import Stats from "three/addons/libs/stats.module.js";
+import {createO2,createCO2,createN2, createH20}from './atome.js';
+
+
+//debug
+let stats = Stats()
+document.body.appendChild(stats.dom)
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+//ambient light
+const ambientLight = new THREE.AmbientLight()
+ambientLight.color = new THREE.Color(0xffffff)
+ambientLight.intensity = 10
+scene.add(ambientLight)
+
+//
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+
+
+// Mesh
+const count =500;
+const countLiquid = 2000;
+
+const radius = 1;
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(radius, 32, 32),
+    new THREE.MeshBasicMaterial({ color: 0xFFFFFFF, wireframe: false , transparent: true, opacity: 0.4})
+)
+
+scene.add(sphere)
+
+const probAtome = {
+    "ox": 20,
+    "ca": 5,
+    "na": 75
+}
+
+// let color = 0xFFFFFF;
+let oxygene = 0;
+let carbon = 0;
+let azote = 0;
+
+let isCarbon = false;
+let cube; // Déclaration de la variable cube
+
+const liquidGroup = new THREE.Group()
+for (var i = 0; i < countLiquid; i++) {
+    const cube = createH20();
+
+
+    cube.position.x = Math.sin(Math.random() * 5 - 1) * 5;
+    cube.position.y = Math.sin(Math.random() * 5 - 1)* 5;
+    cube.position.z = Math.sin(Math.random() * 5 - 1) * 5;
+
+    if (cube.position.distanceTo(sphere.position) <= radius) {
+        cube.position.x = Math.sin(Math.random() * 5 - 1) * 5;
+        cube.position.y = Math.sin(Math.random() * 5 - 1)* 5;
+        cube.position.z = Math.sin(Math.random() * 5 - 1) * 5;
+}
+
+    liquidGroup.add(cube);
+}
+
+const spheregroup = new THREE.Group()
+for (var i = 0; i < count; i++) {
+    const rand = Math.random() * 100;
+    if (rand < probAtome.na ) {
+        cube = createN2();
+        azote++;
+        isCarbon = false;
+    }
+    else if (rand < (probAtome.na + probAtome.ox)) {
+        cube = createO2();
+        oxygene++;
+        isCarbon = false;
+    }
+    else {  
+        isCarbon = true;
+        cube = createCO2();
+        carbon++;
+    }
+    
+    const r = Math.cbrt(Math.random()) * radius	; //radius
+    const angleY = 2 * Math.PI * Math.random(); // angle around Y
+    const angleZ = Math.acos(2 * Math.random() - 1); // angle from Z down 
+
+    // convert spherical coordinates
+    cube.position.x = r * Math.sin(angleZ) * Math.cos(angleY);
+    cube.position.y = r * Math.sin(angleZ) * Math.sin(angleY);
+    cube.position.z = r * Math.cos(angleZ);
+    cube.isCarbon = isCarbon;
+
+    spheregroup.add(cube);
+}
+
+console.log("azote: " + azote + " oxygene: " + oxygene + " carbon: " + carbon);
+scene.add(spheregroup, liquidGroup)
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+//camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+camera.lookAt(new THREE.Vector3(0, 0, 0))
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor( 0xf0a000);
+
+
+/**
+ * Debug
+ */
+const gui = new GUI()
+gui.add(sphere, 'visible').name('sphere')
+gui.add(probAtome, 'ca').min(0).max(100).step(1).name('carbon').onChange()
+console.log(liquidGroup)
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    //update stats
+    stats.update()
+
+    // Update objects
+spheregroup.children.forEach(element => {
+    element.rotation.y = (elapsedTime * 2.5);
+    element.rotation.x = (elapsedTime * 2.5);
+    element.rotation.z = (elapsedTime * 2.5);
+
+    if (element.name == "") {
+        if (element.velocity === undefined){
+            element.velocity = new THREE.Vector3((Math.random() - 0.5) * 0.006, (Math.random() - 0.5) * 0.006, (Math.random() - 0.5) * 0.006);
+        }}
+
+    element.position.add(element.velocity);
+    if (element.isCarbon !== true) {
+        if (element.position.distanceTo(sphere.position) >= radius) {
+            element.velocity.negate();
+            element.position.add(element.velocity);
+    }}
+        else{
+            if (element.position.distanceTo(sphere.position) > radius + 0.5) {
+                element.velocity.set(0,0,0);
+                
+                }
+            }
+});
+
+liquidGroup.children.forEach(element => {
+    element.rotation.y = (elapsedTime * 2.5);
+    element.rotation.x = (elapsedTime * 2.5);
+    element.rotation.z = (elapsedTime * 2.5);
+
+    element.velocity = new THREE.Vector3((Math.random() - 0.5) * 0.02, (Math.random() - 0.5) * 0.02, (Math.random() - 0.5) * 0.02);
+    element.position.add(element.velocity);
+    });
+
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
